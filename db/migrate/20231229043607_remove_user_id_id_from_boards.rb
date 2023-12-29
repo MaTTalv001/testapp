446cbebaecb6f6da5ab37825a595d7f31a8ce51d
MaTTalv001 <pomeranian1981@gmail.com>
2023-12-30 03:10:32 +0900

    postgres

--- db/migrate/20231229043607_remove_user_id_id_from_boards.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdIdFromBoards < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :boards, :user_id_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_29_053920) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.text "body", null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
