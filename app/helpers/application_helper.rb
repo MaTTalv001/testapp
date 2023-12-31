66d0f10f5b468f14554f346e1015107d07a841bb
MaTTalv001 <pomeranian1981@gmail.com>
2023-12-31 22:38:58 +0900

    本番環境のストレージ設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def image_save_path_for(image_name)
+    if Rails.env.production?
+      "/var/lib/data/generated_images/#{image_name}" # 本番環境でのURL
+    else
+      Rails.root.join('public', 'generated_images', image_name) # 開発環境でのローカルパス
+    end
+  end
+
+  def image_load_path_from(image_name)
+    if Rails.env.production?
+      "/var/lib/data/generated_images/#{image_name}" # 本番環境でのURL
+    else
+      "/generated_images/#{image_name}" # 開発環境でのローカルパス
+    end
+  end
 end
